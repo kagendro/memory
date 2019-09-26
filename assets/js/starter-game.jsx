5e8232a3983512fcf77559874b5ea5a9f5de060e
kagendro <kevin.gendron182@gmail.com>
2019-09-26 18:32:33 -0400

    updated the assets.js folder

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -6,41 +6,89 @@ export default function game_init(root) {
   ReactDOM.render(<Starter />, root);
 }
 
+{/* Function iterated from: https://reactjs.org/tutorial/tutorial.html*/}
+function Square(props) {
+  return (
+    <button 
+      className="square" 
+      onClick={props.onClick}>
+      {props.value}
+    </button>
+  );
+}
+
+
+
 class Starter extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { left: false };
+    this.state = {
+      squares: Array(16).fill(null),
+      noGuess: true;
+    };
   }
 
   swap(_ev) {
     let state1 = _.assign({}, this.state, { left: !this.state.left });
     this.setState(state1);
   }
+  
+  handleClick(i) {
+    const squares = this.state.squares.slice();
+    if (this.state.noGuess) {
+      {/* If the user hasn't guessed yet*/}
+      guess1 = this.state.squares[i];
+    } else {
+      {/*two conditions: 
+      1. guess is correct
+      2. guess is incorrect
+      */}
+      if (guess1 == this.state.squares[i]) {
+        {/* TODO Reveal the two squares */}
+      } else {
+        {/* TODO Hide the two squares */}
+      }
+    }
+  }
 
-  hax(_ev) {
-    alert("hax!");
+
+  
+  renderSquare(i) {
+    return (
+      <Square value={this.state.squares[i]}
+      onClick={() => this.handleClick(i)}
+      />
+    );
   }
 
   render() {
-    let button = <div className="column" onMouseMove={this.swap.bind(this)}>
-      <p><button onClick={this.hax.bind(this)}>Click Me</button></p>
-    </div>;
 
-    let blank = <div className="column">
-      <p>Nothing here.</p>
-    </div>;
-
-    if (this.state.left) {
-      return <div className="row">
-        {button}
-        {blank}
+    return (
+      <div className="row">
+        {this.renderSquare(1)}
+        {this.renderSquare(2)}
+        {this.renderSquare(3)}
+        {this.renderSquare(4)}
       </div>;
-    }
-    else {
-      return <div className="row">
-        {blank}
-        {button}
+      <div className="row">
+        {this.renderSquare(5)}
+        {this.renderSquare(6)}
+        {this.renderSquare(7)}
+        {this.renderSquare(8)}
       </div>;
+      <div className="row">
+        {this.renderSquare(9)}
+        {this.renderSquare(10)}
+        {this.renderSquare(11)}
+        {this.renderSquare(12)}
+      </div>;
+      <div className="row">
+        {this.renderSquare(13)}
+        {this.renderSquare(14)}
+        {this.renderSquare(15)}
+        {this.renderSquare(16)}
+      </div>;
+     );
     }
   }
 }
