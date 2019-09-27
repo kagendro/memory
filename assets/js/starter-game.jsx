53fa53cfde85336b9628d4c1e0646aa2191dbdde
kagendro <kevin.gendron182@gmail.com>
2019-09-27 17:38:23 -0400

    Updated starter.jsx and app.css

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -10,7 +10,7 @@ export default function game_init(root) {
 function Square(props) {
   return (
     <button 
-      className="square" 
+      className="Square" 
       onClick={props.onClick}>
       {props.value}
     </button>
@@ -24,69 +24,109 @@ class Starter extends React.Component {
     super(props);
     this.state = {
       squares: ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'],
-      finalAss: [],
+      finalBoard: [],
+      randomAss: [],
       openSquares: [],
     };
+    this.start();
   }
 
 
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (this.state.noGuess) {
-      {/* If the user hasn't guessed yet*/}
-      guess1 = this.state.squares[i];
+  handleClick(name, index) {
+
+    {/* If two guesses have been made*/}
+    if (this.state.openSquares.length == 2) {
+        setTimeout(() => {
+          this.isEqual()
+        },750)
     } else {
-      {/*two conditions: 
-      1. guess is correct
-      2. guess is incorrect
-      */}
-      if (guess1 == this.state.squares[i]) {
-        {/* TODO Reveal the two squares */}
-      } else {
-        {/* TODO Hide the two squares */}
+      let square = {
+        name,
+	index,
+    }
+    let finalBoard = this.state.finalBoard
+    let squares = this.state.openSquares
+    finalBoard[index].close = false
+    squares.push(square)
+    this.setState({
+      openSquares: squares,
+      finalBoard: finalBoard,
+    })
+    if (this.state.openSquares.length == 2) {
+       setTimeout(() => {
+         this.isEqual()
+         },750)   
       }
+   }
+}
+
+/* Check to see if the values of the two flipped squares are equal*/
+  isEqual(){
+    let finalBoard = this.state.finalBoard
+    if((this.state.openSquares[0].name == this.state.openSquares[1].name) && (this.state.openSquares[0].index != this.state.openSquares[1])){
+     finalBoard[this.state.openSquares[0].index].complete = true
+     finalBoard[this.state.openSquares[1].index].complete = true
+    }else{
+     finalBoard[this.state.openSquares[0].index].close = true
+     finalBoard[this.state.openSquares[1].index].close = true
+    }
+    this.state({
+      finalBoard,
+      openSquares: [],
+    })
+
+}
+
+  start(){
+    let finalBoard = []
+    this.state.randomAss = this.randomizeValues(this.state.squares)
+    this.state.randomAss.map((name, index) => {
+      finalBoard.push({
+	name,
+
+        complete: false,
+        close: true,
+      })
+    })
+  this.state.finalBoard = finalBoard
+  }
+   
+  randomizeValues(arr) {
+    let currentIndex = arr.length, randomIndex, temp;
+    let history = [];
+    while(currentIndex !== 0){
+      currentIndex--;
+      randomIndex = Math.floor(Math.random * (currentIndex + 1));
+      temp = arr[currentIndex]
+      arr[currentIndex] = arr[randomIndex];
+      arr[randomIndex] = temp;
     }
   }
-
-
   
   renderSquare(i) {
     return (
       <Square value={this.state.squares[i]}
-      onClick={() => this.handleClick(i)}
+      onClick={() => this.handleClick(name, index)}
       />
     );
   }
 
   render() {
-
     return (
-      <div className="row">
-        {this.renderSquare(1)}
-        {this.renderSquare(2)}
-        {this.renderSquare(3)}
-        {this.renderSquare(4)}
-      </div>;
-      <div className="row">
-        {this.renderSquare(5)}
-        {this.renderSquare(6)}
-        {this.renderSquare(7)}
-        {this.renderSquare(8)}
-      </div>;
-      <div className="row">
-        {this.renderSquare(9)}
-        {this.renderSquare(10)}
-        {this.renderSquare(11)}
-        {this.renderSquare(12)}
-      </div>;
-      <div className="row">
-        {this.renderSquare(13)}
-        {this.renderSquare(14)}
-        {this.renderSquare(15)}
-        {this.renderSquare(16)}
-      </div>;
-     );
+      <div className="starter">
+      {
+        this.state.finalBoard.map((square, index) => {
+          return 
+	    <Square square={square.name} 
+	    click={this.handleclick(square.name, index)} 
+	    close={square.close} 
+	    complete={square.complete}
+	    />
+	  })
+      }
+      </div>
+      )
     }
   }
-}
+
 
