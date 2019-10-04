bb581883253f80bb7a4ee1d29ad2398aa9f4c042
kagendro <45953587+kagendro@users.noreply.github.com>
2019-10-04 13:50:11 -0400

    Updated starter-game.js

--- assets/js/starter-game.jsx
+++ assets/js/starter-game.jsx
@@ -17,7 +17,8 @@ export default function game_init(root) {
 
 const consTiles = ["A","B","C","D","E","F","G","H","A","B","C","D","E","F","G","H"];
 
-function randify(array) {
+// SITE: https://javascript.info/task/shuffle
+function shuffle(array) {
   return array.sort(() => Math.random() - 0.5);
 }
 
@@ -26,209 +27,142 @@ class Starter extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      tiles: randify(consTiles),
+      tiles: shuffle(consTiles),
       score: 0,
-      openSquares: [],
-      show: true
+      openedTiles: [],
+      finalizedTiles: [],
+      matches: 0,
+      maxMatches: 8
     };
+    this.start();
   }
 
-  displayTile(tile){
-    if(true){
-      return tile
-    } else {
+  start(){
+    let finalizedTiles = [];
+    this.state.tiles.map((tile) => {
+      finalizedTiles.push({
+        tile,
+        close: true,
+        complete: false,
+      })
+    })
+    this.state.finalizedTiles = finalizedTiles;
+  }
 
+    handleClick = (tile,key) => {
+        console.log("Tile: " + tile + " Key: " + key)
+        let matchesSoFar = this.state.matches;
+        if (matchesSoFar !== this.state.maxMatches){
+            if(this.state.openedTiles.length === 2){
+                setTimeout(() => {
+                this.check()
+                },750)
+            }else {
+                let open = {
+                    tile,
+                    key
+                }
+                let finalizedTiles = this.state.finalizedTiles
+                let openTiles = this.state.openedTiles
+                finalizedTiles[key].close = false
+                openTiles.push(open)
+                this.setState({
+                openedTiles: openTiles,
+                finalizedTiles: finalizedTiles
+                })
+                if(this.state.openedTiles.length == 2){
+                setTimeout(() => {
+                    this.check()
+                },750)
+                }
+            }
+        } 
     }
-  }
 
+    check(){
+      let finalizedTiles = this.state.finalizedTiles
+      if((this.state.openedTiles[0].tile == this.state.openedTiles[1].tile) && (this.state.openedTiles[0].key !== this.state.openedTiles[1].key)
+        && (finalizedTiles[this.state.openedTiles[0].key].complete !== true)) {
+        finalizedTiles[this.state.openedTiles[0].key].complete = true
+        finalizedTiles[this.state.openedTiles[1].key].complete = true
+        this.incrementScore();
+
+
+      }else {
+        finalizedTiles[this.state.openedTiles[0].key].close = true
+        finalizedTiles[this.state.openedTiles[1].key].close = true
+        this.decrementScore();
+      }
+      this.setState({
+        finalizedTiles,
+        openedTiles: []
+      })
+      console.log("openTiles is empty again!")
+    }
   
 
-  removeTiles(tile){
-    return this.createTiles().filter((item)=> {
-        return item.props.children == tile
-    })
-  }
+    incrementScore() {
+        this.setState({
+            score: this.state.score + 5,
+            matches: this.state.matches + 1
+        })
 
-  createTiles(){
-    return this.state.tiles.map((tile,key) => {
-      return(
-        <button key={key}
-        className="button button-outline column tile"
-        onClick={() => {
-          this.setState({
-            score: this.state.score + 1
-          })
-        }}>
-          {this.displayTile(tile)}
-        </button>
-      )
-    })
-  }
+        console.log("Number of matches: " + this.state.matches)
+    }
 
+    decrementScore() {
+        let yourScore = this.state.score;
+        if (yourScore > 1){
+            this.setState({
+                score: this.state.score - 2,
+            })
+        return;
+        }if(yourScore == 1){
+            this.setState({
+                score: this.state.score - 1,
+            })
+        }
+    }
+
+    displayTile(tile,key){
+        if((this.state.finalizedTiles[key].close == false)||
+            (this.state.finalizedTiles[key].complete ==true)){
+          return tile
+        }
+      }
 
   render() {
     return (
       <div className="starter">
         <div className="row board">
-        {console.log("Displays created tiles: ",this.createTiles())}
-          {this.createTiles()}
+
+        {this.state.tiles.map((tile,key) => {
+            return <button 
+                key={key}
+                className="button button-outline column tile"
+                onClick = {() => {this.handleClick(tile,key)}}
+                >
+                {this.displayTile(tile, key)}
+            </button>
+        })}
+    
         </div>
         <button
-        onClick={()=>{
-          this.setState({
-            tiles: randify(consTiles),
-            score: 0
-          })
-        }}>Reset
+            onClick={()=>{
+                this.setState({
+                    tiles: shuffle(consTiles),
+                    score: 0,
+                    matches: 0,
+                    openedTiles: [],
+                    finalizedTiles: []
+                })
+            }}>Reset
         </button>
-        <p>Score: {this.state.score}</p>
-      </div>
+        <p>Your Score: {this.state.score}</p>
+        {this.checkWinner}
+    </div>
     );
   }
 }
 
-/* import React from 'react';
-import ReactDOM from 'react-dom';
-import _ from 'lodash';
-
-export default function game_init(root) {
-  ReactDOM.render(<Starter />, root);
-*/
-/*Referenced sources:
- *https://reactjs.org/tutorial/tutorial.html
- *https://www.youtube.com/watch?v=ZniVgo8U7ek
- *https://codeburst.io/learning-react-js-by-building-a-minesweeper-game-ced9d41560ed
- *https://codepen.io/yigitcukuren/pen/GyxxVm?editors=0010
- *https://stackoverflow.com/questions/5836833/create-an-an-array-with-random-values
-*/
-/*
-
-const squares: ['A', 'A', 'B', 'B', 'C', 'C', 'D', 'D', 'E', 'E', 'F', 'F', 'G', 'G', 'H', 'H'];
-
-
-class Starter extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      finalBoard: [],
-      randomAss: [],
-      openSquares: [],
-    };
-    this.start();
-  }
-
-
-  handleClick(name, index) {
-
-    if (this.state.openSquares.length == 2) {
-        setTimeout(() => {
-          this.isEqual()
-        },750)
-    } else {
-      let square = {
-        name,
-	index,
-    }
-    let finalBoard = this.state.finalBoard
-    let squares = this.state.openSquares
-    finalBoard[index].close = false
-    squares.push(square)
-    this.setState({
-      openSquares: squares,
-      finalBoard: finalBoard,
-    })
-    if (this.state.openSquares.length == 2) {
-       setTimeout(() => {
-         this.isEqual()
-         },750)   
-      }
-    }
-  }
-
-  isEqual(){
-    let finalBoard = this.state.finalBoard
-    if((this.state.openSquares[0].name == this.state.openSquares[1].name) && (this.state.openSquares[0].index != this.state.openSquares[1])){
-     finalBoard[this.state.openSquares[0].index].complete = true
-     finalBoard[this.state.openSquares[1].index].complete = true
-    }else{
-     finalBoard[this.state.openSquares[0].index].close = true
-     finalBoard[this.state.openSquares[1].index].close = true
-    }
-    this.state({
-      finalBoard,
-      openSquares: [],
-    })
-
-  }
-
-  start(){
-    console.log("Hi")
-    let finalBoard = []
-    this.state.randomAss = this.randomizeValues(this.state.squares)
-    this.state.randomAss.map((name, index) => {
-      finalBoard.push({
-	name,
-        complete: false,
-        close: true,
-      })
-    })
-  this.state.finalBoard = finalBoard
-  }
-   
-  randomizeValues(arr) {
-    let currentIndex = arr.length, randomIndex, temp;
-    while(0 !== currentIndex){
-      currentIndex -= 1;
-      randomIndex = Math.floor(Math.random * (currentIndex + 1));
-      temp = arr[currentIndex]
-      arr[currentIndex] = arr[randomIndex];
-      arr[randomIndex] = temp;
-    }
-  }
-
-  render() {
-    return (
-      <div className="starter">
-      {
-        this.state.finalBoard.map((square, index) => {
-          return 
-	    <Square square={square.name} 
-	    click={this.handleclick(square.name, index)} 
-	    close={square.close} 
-	    complete={square.complete}
-	    />
-	  })
-      }
-      </div>
-      )
-    }
-  }
-
-class Square extends React.Component {
-   constructor(props){
-      super(props)
-      this.state = {
-
-      }
-   }
-   clicked(square){
-      this.props.click(square)
-   }
-   render(){
-     return(
-       <div className={"square" + (!this.props.close ? ' opened' : '') + (this.props.complete ? ' matched' : '')} onClick ={() => this.clicked(this.props.square)}>
-         <div className="front">
-           ?
-         </div>
-         <div className="back">
-         </div>
-       </div>
-
-     )
-   } 
- }
-   
-}
-
-*/
 
